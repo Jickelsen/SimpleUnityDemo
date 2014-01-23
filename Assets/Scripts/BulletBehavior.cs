07e3687f2b8a9a2220a07f446cd81a10a1646b70
Jacob Michelsen <jickelsen@rakethopp.se>
2014-01-23 16:56:43 +0100

    Added the Unity project files.

---
+++ Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehavior : MonoBehaviour {
+
+	public float velocityFactor;
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Rigidbody>().velocity = this.transform.forward*velocityFactor;
+		Debug.Log ("I'm flying!");
+		GameObject.Destroy (this.gameObject, 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour {
+
+	public GameObject bullet;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown("Fire1")) {
+			GameObject myBullet;
+			myBullet = GameObject.Instantiate(bullet, this.transform.position+this.transform.forward*0.5f, this.transform.rotation) as GameObject;
+		}	
+
+	}
+}
